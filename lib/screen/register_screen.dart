d885bbfc1ae31a0dfab54e60c27b4abf41cc0e13
Mahmoud <modemushtaha@gmail.com>
2023-11-03 07:19:57 +0300

    E-commerce App

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,3 +1,5 @@
+import 'package:evgo/screen/register_screen.dart';
+import 'package:evgo/widget/circle_widget.dart';
 import 'package:evgo/widget/header_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -15,27 +17,57 @@ class RegisterScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           HeaderWidget(width: width, height: height),
-          const SizedBox(height: 30,),
-          const Text('Register Account',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-          SizedBox(
-            width: width*0.5,
-            child: const Text('Complete your details or continue with social media',
-                style: TextStyle(color: Colors.grey),
-              textAlign: TextAlign.center,
-            ),
+          Column(
+            children: [
+              const SizedBox(height: 30,),
+              const Text('Register Account',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+              SizedBox(
+                width: width*0.5,
+                child: const Text('Complete your details or continue with social media',
+                  style: TextStyle(color: Colors.grey),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: height*0.06,),
-          buildTextFormField(width,'Email', Icons.email_outlined,'Enter your email'),
-          SizedBox(height: height*0.03,),
-          buildTextFormField(width,'Password', Icons.lock_outline,'Enter your Password'),
-          SizedBox(height: height*0.03,),
-          buildTextFormField(width,'Confirm password', Icons.lock_outline,'Re-Enter your Password'),
-          SizedBox(height: height*0.04,),
-          ButtonWidget(width: width,callBack: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => CompleteProfileScreen(),));
-          },)
+          Column(
+            children: [
+              SizedBox(height: height*0.06,),
+              buildTextFormField(width,'Email', Icons.email_outlined,'Enter your email'),
+              SizedBox(height: height*0.03,),
+              buildTextFormField(width,'Password', Icons.lock_outline,'Enter your Password'),
+              SizedBox(height: height*0.03,),
+              buildTextFormField(width,'Confirm password', Icons.lock_outline,'Re-Enter your Password'),
+              SizedBox(height: height*0.04,),
+              ButtonWidget(width: width,callBack: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => CompleteProfileScreen(),));
+              },),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleWidget(width: width,url: 'images/google.webp',),
+                  SizedBox(width: width*0.05,),
+                  CircleWidget(width: width,url: 'images/unnamed.png',),
+                  SizedBox(width: width*0.05,),
+                  CircleWidget(width: width,url: 'images/Logo_of_Twitter.svg.webp',)
+
+                ],
+              ),
+              SizedBox(height: height*0.05,),
+              const Text('By continuing your confirm that you agree',
+                style: TextStyle(color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 5,)
+            ],
+          )
         ],
       ),
     );
@@ -62,3 +94,5 @@ class RegisterScreen extends StatelessWidget {
         );
   }
 }
+
+
